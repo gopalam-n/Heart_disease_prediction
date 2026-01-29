93ba3555dea1e9695f5f97eb9c0d0c2fcf7853b6
gopalam-n <165139121+gopalam-n@users.noreply.github.com>
2026-01-29 16:28:02 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from predictor.forms import HeartDiseaseForm
+
+
+def heart(request):
+   
+    df = pd.read_csv('static/Heart_train.csv')
+    data = df.values
+    X = data[:, :-1]
+    Y = data[:, -1:]
+
+    value = ''
+
+    if request.method == 'POST':
+
+        age = float(request.POST['age'])
+        sex = float(request.POST['sex'])
+        cp = float(request.POST['cp'])
+        trestbps = float(request.POST['trestbps'])
+        chol = float(request.POST['chol'])
+        fbs = float(request.POST['fbs'])
+        restecg = float(request.POST['restecg'])
+        thalach = float(request.POST['thalach'])
+        exang = float(request.POST['exang'])
+        oldpeak = float(request.POST['oldpeak'])
+        slope = float(request.POST['slope'])
+        ca = float(request.POST['ca'])
+        thal = float(request.POST['thal'])
+
+        user_data = np.array(
+            (age,
+             sex,
+             cp,
+             trestbps,
+             chol,
+             fbs,
+             restecg,
+             thalach,
+             exang,
+             oldpeak,
+             slope,
+             ca,
+             thal)
+        ).reshape(1, 13)
+
+        rf = RandomForestClassifier(
+            n_estimators=16,
+            criterion='entropy',
+            max_depth=9
+        )
+
+        rf.fit(np.nan_to_num(X), Y)
+        rf.score(np.nan_to_num(X), Y)
+        predictions = rf.predict(user_data)
+
+        if int(predictions[0]) == 1:
+            value = 'have'
+        elif int(predictions[0]) == 0:
+            value = "don\'t have"
+
+    return render(request,
+                  'heart.html',
+                  {
+                      'context': value,
+                      'title': 'Heart Disease Prediction',
+                      'active': 'btn btn-success peach-gradient text-white',
+                      'heart': True,
+                      'form': HeartDiseaseForm(),
+                  })
+def home(request):
+
+    return render(request,
+                  'home.html')
+
+
+
+def handler404(request):
+    return render(request, '404.html', status=404)
